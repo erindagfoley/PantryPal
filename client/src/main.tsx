57d326897e022c76709554114d807a63233dbd61
erin foley <erindagfoley@gmail.com>
2025-02-19 10:02:22 -0500

    eliminated nodemon crashing error

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 
+
 import App from './App.tsx';
 import ErrorPage from './pages/ErrorPage.tsx';
 import Home from './pages/Home.tsx';
@@ -9,33 +10,36 @@ import Login from './pages/Login.tsx';
 import SignUp from './pages/SignUp.tsx';
 import RecipeSearch from './components/RecipeSearch';
 
+
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        index: true,
-        element: <Home />
-      },
-      {
-        path: '/login',
-        element: <Login />
-      },
-      {
-        path: '/signup',
-        element: <SignUp />
-      },
-      {
-        path: '/search',
-        element: <RecipeSearch />
-      },
-    ]
-  }
+ {
+   path: '/',
+   element: <App />,
+   errorElement: <ErrorPage />,
+   children: [
+     {
+       index: true,
+       element: <Home />
+     },
+     {
+       path: '/login',
+       element: <Login />
+     },
+     {
+       path: '/signup',
+       element: <SignUp />
+     },
+     {
+       path: '/search',
+       element: <RecipeSearch />
+     },
+   ]
+ }
 ]);
+
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
+ ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
 }
+
